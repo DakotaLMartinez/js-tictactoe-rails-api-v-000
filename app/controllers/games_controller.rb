0bf65bdf8a5d2a2401e82c43f93a8bacd9b61ece
Dakota Martinez <dakotaleemusic@gmail.com>
2016-05-18 07:58:44 +0000

    adds routes for games and game serializer

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,24 @@
+class GamesController < ApplicationController
+  def index 
+    @games = Game.all 
+    render json: @games, status: 200
+  end
+  
+  def create 
+    @game = Game.create(game_params)
+    render json: @game, status: 201
+  end 
+  
+  def update 
+    @game = Game.find(params[:id])
+    @game.update(game_params)
+    render json: @game, status: 200
+  end 
+  
+  private 
+  
+  def game_params
+    params.require(:game).permit(:state)
+  end
+  
+end
